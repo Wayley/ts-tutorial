30e42484f30e28920878fa3257aec7e30c340dbe
wzheng <hb_wangzheng@163.com>
2019-01-11 18:39:40 +0800

    add pages

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
+import { sayHello } from './greet'
+
 function hello(from: string) {
   console.log('-----', `Hello from ${from}`)
 }
 hello('TYPESCRIPT')
+const message = sayHello('ts')
+console.log(message)
+
+function showHello(divName: string, name: string) {
+  const elt = document.getElementById(divName)
+  elt.innerText = sayHello(name)
+}
+const eleId = 'greeting'
+showHello(eleId, 'TypeScript')
+/*  */
