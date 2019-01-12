40a7ccb0788809e770b4f7938cacc486e6aa5ba8
wzheng <hb_wangzheng@163.com>
2019-01-12 12:04:27 +0800

    add Watchify to the project

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,12 @@ var gulp = require('gulp')
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
 var tsify = require('tsify')
+var watchify = require('watchify')
+var gutil = require('gulp-util')
 
 var paths = {
   pages: ['src/*.html']
 }
-gulp.task('copy-html', function() {
-  return gulp.src(paths.pages).pipe(gulp.dest('dist'))
-})
 
 var browserifyParams = {
   basedir: '.',
@@ -17,12 +16,20 @@ var browserifyParams = {
   cache: {},
   packageCache: {}
 }
+var watchedBrowserify = watchify(browserify(browserifyParams).plugin(tsify))
 
-gulp.task('default', ['copy-html'], function() {
-  return browserify(browserifyParams)
-    .plugin(tsify)
+gulp.task('copy-html', function() {
+  return gulp.src(paths.pages).pipe(gulp.dest('dist'))
+})
+
+function bundle() {
+  return watchedBrowserify
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('dist'))
-})
+}
+
+gulp.task('default', ['copy-html'], bundle)
+watchedBrowserify.on('update', bundle)
+watchedBrowserify.on('log', gutil.log)
 // /*  */
