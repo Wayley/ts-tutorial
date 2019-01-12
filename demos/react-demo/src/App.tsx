39d7da2c5639472242cfe7fd6e80ce1c9793def7
wzheng <hb_wangzheng@163.com>
2019-01-12 16:41:24 +0800

    add react demo into the project

---
+++ demos/react-demo/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className='app'>
+        <header className='app-header'>header</header>
+        <div className='app-main'>ma2in</div>
+      </div>
+    );
+  }
+}
+
+export default App;
