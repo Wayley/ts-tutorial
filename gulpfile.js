d197d77eb6e9aaba548c0f87b06e79714155046b
wzheng <hb_wangzheng@163.com>
2019-01-12 11:50:46 +0800

    add Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,17 @@ var paths = {
 gulp.task('copy-html', function() {
   return gulp.src(paths.pages).pipe(gulp.dest('dist'))
 })
+
+var browserifyParams = {
+  basedir: '.',
+  debug: true,
+  entries: ['src/main.ts'],
+  cache: {},
+  packageCache: {}
+}
+
 gulp.task('default', ['copy-html'], function() {
-  return browserify({
-    basedir: '.',
-    debug: true,
-    entries: ['src/main.ts'],
-    cache: {},
-    packageCache: {}
-  })
+  return browserify(browserifyParams)
     .plugin(tsify)
     .bundle()
     .pipe(source('bundle.js'))
