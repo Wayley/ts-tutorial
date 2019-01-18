52321050f58234b2c1b0f485e06ce5895f21a265
wzheng <hb_wangzheng@163.com>
2019-01-18 17:30:45 +0800

    修改app

--- demos/angular2/src/app/app.component.ts
+++ demos/angular2/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavService } from './nav.service';
+
 import { Nav } from './types/nav';
 @Component({
   selector: 'app-root',
@@ -9,19 +10,11 @@ import { Nav } from './types/nav';
 })
 export class AppComponent {
   title = '管理系统';
-  navList = [
-    { id: 1, name: '用户管理' },
-    { id: 2, name: '学习管理' },
-    { id: 3, name: '统计管理' },
-    { id: 4, name: '系统管理' }
-  ];
-
-  navList_: Nav[] = [];
+  navList: Nav[] = [];
   constructor(private navService: NavService) {}
 
   getNavList(): void {
-    this.navList_ = this.navService.getNavList();
-    console.log(this.navList_, '-----------');
+    this.navList = this.navService.getNavList();
   }
 
   ngOnInit() {
--- demos/angular2/src/app/app.module.ts
+++ demos/angular2/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { NavComponent } from './nav/nav.component';
 import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CourseListComponent } from './views/course/course-list/course-list.component';
+import { CourseEditComponent } from './views/course/course-edit/course-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     NavComponent,
-    LoginComponent
+    LoginComponent,
+    CourseListComponent,
+    CourseEditComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
